9bb35fc5bdf02b3d03c4b5a2248b47072fac91cc
betoFlores58 <albertfr@live.com.mx>
2021-06-15 14:38:24 -0500

    herokuapp

--- liga/settings.py
+++ liga/settings.py
@@ -95,9 +95,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'liga.wsgi.application'
 
-ALLOWED_HOSTS ={
-    '.herokuapp.com','localhost','127.0.0.1','0.0.0.0'
-}
+ALLOWED_HOSTS = ['.herokuapp.com','localhost','127.0.0.1','0.0.0.0']
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
