b12abb27b95817540e7396d6e5a25f37a89c3b6b
betoFlores58 <albertfr@live.com.mx>
2021-06-15 15:33:23 -0500

    -

--- liga/settings.py
+++ liga/settings.py
@@ -1,4 +1,6 @@
 from pathlib import Path, os
+import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -175,5 +177,4 @@ STRIPE_TEST_PUBLISHABLE_KEY=os.environ.get('STRIPE_TEST_PUBLISHABLE_KEY')
 STRIPE_TEST_SECRET_KEY=os.environ.get('STRIPE_TEST_SECRET_KEY')
 
 #Heroku
-import dj_database_url
-DATABASES = {'default':dj_database_url.config()}
+DATABASES['default'] =  dj_database_url.config()
